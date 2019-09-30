3fd6cdab549bdb253636dd86f5c6eb799e56f5b3
calderonra <00004017@uca.edu.sv>
2019-09-30 10:46:38 -0600

    Primera parte

---
+++ Labo5EnPython/labo5_01.py
@@ -0,0 +1,10 @@
+print("Ingree el numero del cual quiere saber el factorial")
+x=int(input())
+acum=1
+for i in range(1,x+1):
+    acum=i*acum
+
+    
+
+    
+print(acum)    
---
+++ Labo5EnPython/labo5_02.py
@@ -0,0 +1,13 @@
+print("Ingrese el numero: ")
+x=int(input())
+fibbo=1
+for i in range(1,x+1):
+    if i==0:
+        fibbo=0
+    if i==1 or i==2:
+        fibbo=1
+    fibbo=    
+
+    pass
+
+print(fibbo)
